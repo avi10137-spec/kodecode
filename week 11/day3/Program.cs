dc4a66329e55db2d408afcd4fca61e57c6408150
avi <avi10137@gmail.com>
2026-06-28 15:01:56 +0300

    create propertis in class

--- week 11/day3/Program.cs
+++ week 11/day3/Program.cs
@@ -1,1 +1,175 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Net.NetworkInformation;
+namespace day3;
+
+enum Statuses { Friendly, Hostile, Uniidentified }
+class Intelligence
+{
+  
+    static void addMassege( List<int> ides,List <Statuses> status,List<double?> signal)
+
+    {
+        int newId;
+        Console.WriteLine("enter id");
+        string myid=Console.ReadLine();
+        if (!int.TryParse(myid , out newId))
+        {
+            Console.WriteLine("id invalid input");
+            return;
+        }
+        if (ides.Contains(newId))
+            {
+            Console.WriteLine("id is exist");
+            return;
+        }
+            ides.Add(newId);
+        Statuses newStatus;
+        Console.WriteLine("enter status");
+        string myStatus = Console.ReadLine();
+        if (!Statuses.TryParse(myStatus, out newStatus))
+        {
+            Console.WriteLine("status invalid input");
+            return;
+        }
+
+        status.Add(newStatus);
+        double ? newSignal=null;
+
+        Console.WriteLine("enter signal");
+        string mySignal = Console.ReadLine();
+        if (!string.IsNullOrWhiteSpace(mySignal))
+        {
+            if (!double.TryParse(mySignal, out double newSignal1))
+            {
+                Console.WriteLine("signal invalid input");
+                return;
+            }
+            newSignal = newSignal1;  
+       
+        }
+
+
+        signal.Add(newSignal);
+
+    }
+    static int findById(int id,List<int> ides, List<Statuses> status, List<double?>signal)
+    {
+        for (int i = 0; i < ides.Count; i++)
+        {
+            if (ides[i] == id)
+            {
+                return i;
+            }
+
+        }
+        return -1;
+    }
+    
+    static void update(List<int> ides, List<Statuses> status, List<double?>signal)
+        
+    {
+        int newId;
+        Console.WriteLine("enter id");
+        string myid = Console.ReadLine();
+        if (!int.TryParse(myid, out newId))
+        {
+            Console.WriteLine("id invalid input");
+            return;
+        }
+        if (!ides.Contains(newId))
+        {
+            Console.WriteLine("id is not exist");
+        }
+         int index = findById(newId, ides, status, signal);
+        double? newSignalForUp = null;
+
+        Console.WriteLine("enter  new signal");
+        string mySignal = Console.ReadLine();
+        if (!string.IsNullOrWhiteSpace(mySignal))
+        {
+            if (!double.TryParse(mySignal, out double newSignal1))
+            {
+                Console.WriteLine("signal invalid input");
+                return;
+            }
+            newSignalForUp = newSignal1;
+            signal[index] = newSignalForUp;
+
+        }
+    }
+     static void printerMassege(int index,List<int> ides, List<Statuses> status, List<double?> signal)
+
+
+    //    int newId;
+    //Console.WriteLine("enter id");
+    //    string myid = Console.ReadLine();
+    //    if (!int.TryParse(myid, out newId))
+    //    {
+    //        Console.WriteLine("id invalid input");
+    //        return;
+    //    }
+    //    if (!ides.Contains(newId))
+    //    {
+    //        Console.WriteLine("id is not exist");
+    //        return;
+    //    }
+    //    int index = findById(newId, ides, status, signal);//{
+    { 
+
+Console.WriteLine($"massege id {ides[index]}  status is {status[index]} ,value signal is {signal[index]} ");
+      }
+    static void printAll(List<int> ides, List<Statuses> status, List<double?> signal)
+    {
+      
+        {
+            for (int i = 0; i < ides.Count; i++)
+            {
+                printerMassege(i, ides, status, signal);
+            }
+        }
+    }
+    static void Main()
+    {
+     
+    List<int> ides = new List<int>();
+    List<Statuses> status = new List<Statuses>();
+    List<double?> signal = new List<double?>();
+        string choice = "-1";
+
+        while (choice != "4")
+        {
+            Console.WriteLine("=== Signal Intercept Log ===");
+            Console.WriteLine("1. Add new transmission");
+            Console.WriteLine("2. Calibrate existing signal");
+            Console.WriteLine("3. List all transmissions");
+            Console.WriteLine("4. Quit");
+            Console.Write("Choose option: ");
+
+            choice = Console.ReadLine();
+            switch (choice)
+            {
+                case "1":
+                    addMassege(ides, status, signal);
+                    break;
+                case "2":
+                    update(ides, status, signal);
+                    break;
+                case "3":
+                    printAll(ides, status, signal);
+                    break;
+                case "4":
+                    break;
+                default:
+                    Console.WriteLine("There is no such option, try again.");
+                    break;
+            }
+        }
+        
+
+      
+        //printerMassege(ides, status, signal);
+        
+    }
+}
+
+
---
+++ week12/day1/oop1/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+namespace oop;
+enum AccountTypes{ Checking,Business,Saving}
+class BankAccount
+{
+
+    private string _ownerName;
+    private double _balance;
+    private AccountTypes _accountType;
+    private bool _isActive;
+    private List<string> _transationHistory;
+    public int AccountNumber { get; }
+    public string OwnerName
+    {
+        get => _ownerName;
+        set
+        {
+            if (string.IsNullOrWhiteSpace(value))
+                _ownerName = "Unknown";
+            else
+                _ownerName = value;
+
+
+        }
+        }
+    public double Balance
+    {
+        get => _balance;
+        set
+        {
+            if (value < 0)
+                _balance = 0;
+            else _balance = value;
+        }
+    }
+    public AccountTypes AccountType
+    {
+        get => _accountType;
+        set
+        {
+            
+            
+                _accountType = value;
+            
+            
+
+        }
+
+    }
+
+    public BankAccount(int accountNumber, string ownerName,double balance,AccountTypes accountType = AccountTypes.Checking)
+    {
+        AccountNumber = accountNumber;
+        OwnerName = ownerName;
+        Balance = balance;
+        AccountType = accountType;
+    }
+}
+class Tirgul
+{
+    static void Main()
+    {
+        BankAccount a = new BankAccount(10,"avi",150);
+        Console.WriteLine($" id number {a.AccountNumber} name is {a.OwnerName} balance is {a.Balance} type is {a.AccountType}");
+
+    }
+}
