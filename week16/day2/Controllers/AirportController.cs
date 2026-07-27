55406f8054e5ed411de8e6ec055b941e093d9be0
avi <avi10137@gmail.com>
2026-07-27 11:35:02 +0300

    complete first part of day

---
+++ week16/day2/Controllers/AirportController.cs
@@ -0,0 +1,112 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using AirportFlightLogApi.Models;
+
+namespace AirportFlightLogApi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AirportController : ControllerBase
+    {
+        private static readonly List<FlightLog> _flightLog = new()
+     {
+         new FlightLog
+         {
+             Id = 1,
+            FlightNumber = "AA101",
+            Airline = "American Airlines",
+            Destination = "New York JFK",
+            PassengerCount = 180,
+            ScheduledDeparture = DateTime.UtcNow.AddHours(2),
+            Status = "Scheduled"
+            },
+            new FlightLog
+            {
+            Id = 2,
+            FlightNumber = "BA202",
+            Airline = "British Airways",
+            Destination = "London Heathrow",
+            PassengerCount = 250,
+            ScheduledDeparture = DateTime.UtcNow.AddHours(4),
+            ActualDeparture = DateTime.UtcNow.AddHours(4).AddMinutes(15),
+            Status = "Departed",
+            Remarks = "Delayed due to weather"
+            },
+            new FlightLog
+            {
+            Id = 3,
+            FlightNumber = "LH303",
+            Airline = "Lufthansa",
+            Destination = "Frankfurt",
+            PassengerCount = 200,
+            ScheduledDeparture = DateTime.UtcNow.AddHours(6),
+            Status = "Scheduled"
+            }  
+     };
+        private static int _nextId = 4;
+        [HttpGet]
+        public ActionResult<IEnumerable<FlightLog>> GetAllFlight()
+        {
+            return Ok(_flightLog);
+        }
+        [HttpGet("{id}")]
+        public ActionResult <FlightLog> GetFlightById(int id)
+        {
+            var result = _flightLog.FirstOrDefault(l => l.Id == id);
+            if (result == null)
+            {
+                return NotFound();
+            }
+            return Ok(result);
+        }
+        [HttpPost]
+        public ActionResult <FlightLog> CreateFlightLog(FlightLog flightlog)
+        {
+            flightlog.Id = _nextId++;
+            _flightLog.Add(flightlog);
+            return CreatedAtAction(nameof(GetFlightById), new { id = flightlog.Id }, flightlog);
+
+        }
+        [HttpPut("{id}")]
+        public IActionResult UpdateFLightLog(int id,FlightLog updatedLog)
+        {
+            var existingLog = _flightLog.FirstOrDefault(l => l.Id == id);
+            if(existingLog == null)
+            {
+                return NotFound();
+            }
+            existingLog.FlightNumber = updatedLog.FlightNumber;
+            existingLog.Airline = updatedLog.Airline;
+            existingLog.Destination = updatedLog.Destination;
+            existingLog.PassengerCount = updatedLog.PassengerCount;
+            existingLog.ScheduledDeparture = updatedLog.ScheduledDeparture;
+            existingLog.ActualDeparture = updatedLog.ActualDeparture;
+            existingLog.Remarks = updatedLog.Remarks;
+            existingLog.Status = updatedLog.Status;
+            return NoContent();
+
+        }
+        [HttpGet("search")]
+        public ActionResult<IEnumerable<FlightLog>> GetBySearch([FromQuery] string airline)
+        {
+            if (string.IsNullOrEmpty(airline))
+                {
+                return BadRequest("airline parameter cannot be empty");
+               }
+            var logs = _flightLog.Where(l => l.Airline.Contains(airline, StringComparison.OrdinalIgnoreCase)).ToList();
+            return Ok(logs);
+        }
+        [HttpDelete("{id}")]
+        public IActionResult DeleteFlightLog(int id)
+        {
+            var log = _flightLog.FirstOrDefault(l => l.Id == id);
+            {
+             if(log == null)
+                {
+                    return NotFound();
+                }
+                _flightLog.Remove(log);
+                return NoContent();
+            }
+        }
+    }
+}
---
+++ week16/day2/Models/FlightLog.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace AirportFlightLogApi.Models
+{
+    public class FlightLog
+    {
+        public int Id { get; set; }
+        [Required(ErrorMessage = "Flight number is required")]
+        [StringLength(10, MinimumLength = 3, ErrorMessage = "Flight number must be between 3 and 10 charcters")]
+        public string FlightNumber { get; set; } = string.Empty;
+        [Required(ErrorMessage = "airline is required")]
+        [StringLength(50, ErrorMessage = " airline name cannot exceed 50 charcters")]
+        public string Airline { get; set; } = string.Empty;
+        [Required(ErrorMessage = "Destination is required")]
+        [StringLength(100, ErrorMessage = "Destination cannot exceed 100 characters")]
+        public string Destination { get; set; } = string.Empty;
+        [Range(1,1000,ErrorMessage = " passenger must be between 1 and 1000")]
+        public int PassengerCount { get; set; }
+        [Required(ErrorMessage = "Departure time is required")]
+        public DateTime ScheduledDeparture { get; set; }
+        public DateTime? ActualDeparture { get; set; }
+        [StringLength(500, ErrorMessage = "Remarks cannot exceed 500 characters")]
+
+        public string? Remarks { get; set; }
+
+        public string Status { get; set; } = "Scheduled";
+
+
+    }
+}
